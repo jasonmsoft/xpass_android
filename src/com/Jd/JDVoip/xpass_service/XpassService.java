b758ac32872753e1ad6c42ee1ba24d709c495118
maji <308378975@qq.com>
2015-06-14 15:47:06 +0800

    1.加入service类，用于后面处理注册和接来话

---
+++ src/com/Jd/JDVoip/xpass_service/XpassService.java
@@ -0,0 +1,15 @@
+package com.Jd.JDVoip.xpass_service;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+/**
+ * Created by cdmaji1 on 2015/6/14.
+ */
+public class XpassService extends Service{
+@Override
+public IBinder onBind(Intent intent) {
+	return null;
+}
+}
